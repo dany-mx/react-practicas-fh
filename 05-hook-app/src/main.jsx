41d672a1a782158cd1de16f57c7016919885e462
Gandarillas <gandani.91@gmail.com>
2022-11-30 16:53:01 -0600

    router dom start

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -13,12 +14,20 @@ import ReactDOM from "react-dom/client";
 // import { CallbackHook } from "./06-memos/CallbackHook";
 // import { Padre } from "./07-tarea-memo/Padre";
 // import "../src/08-useReducer/intro-reducer";
+// import { TodoApp } from "./08-useReducer/TodoApp";
 
-import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainApp />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <TodoApp />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );

6376aed49d6ba51b811bcd77f8c24236e6874963
Daniel Gandarillas <gandani.91@gmail.com>
2022-12-14 23:57:30 -0600

    routas multiples

--- 07-heroes-app/src/router/AppRouter.jsx
+++ 07-heroes-app/src/router/AppRouter.jsx
@@ -1,28 +1,18 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Login } from '../auth';
-import { Dc, Marvel } from '../heroes';
-import { Navbar } from '../ui';
+import { HeroesRoutes } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route
-          path="marvel"
-          element={<Marvel />}
-        />
-        <Route
-          path="dc"
-          element={<Dc />}
-        />
         <Route
           path="login"
           element={<Login />}
         />
         <Route
-          path="/"
-          element={<Navigate to="/marvel" />}
+          path="/*"
+          element={<HeroesRoutes />}
         />
       </Routes>
     </>

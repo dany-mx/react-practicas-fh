84696b3cb29719b9a79bef31811bc5b15f3f042b
Daniel Gandarillas <gandani.91@gmail.com>
2022-12-18 05:39:59 -0600

    personalización de tarjetas

--- 07-heroes-app/src/heroes/components/HeroCard.jsx
+++ 07-heroes-app/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+  if (alter_ego === characters) return <></>;
+  return <p>{characters}</p>;
+};
+
 export const HeroCard = ({
   id,
   superhero,
@@ -7,6 +14,7 @@ export const HeroCard = ({
   characters,
 }) => {
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
+  const charactersByHero = <p>{characters}</p>;
   return (
     <div className="col">
       <div className="card">
@@ -22,7 +30,15 @@ export const HeroCard = ({
             <div className="card-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
-              <p>{characters}</p>
+              {/* {alter_ego !== characters && charactersByHero} */}
+              <CharactersByHero
+                alter_ego={alter_ego}
+                characters={characters}
+              />
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+              <Link to={`/hero/${id}`}>Mas...</Link>
             </div>
           </div>
         </div>

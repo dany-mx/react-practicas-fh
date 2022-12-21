002ffd993aaf448dd34232b9fa44060d2639db38
Gandarillas <gandani.91@gmail.com>
2022-12-21 10:46:29 -0600

    Utilizar el contexto en navbar

--- 07-heroes-app/src/ui/components/Navbar.jsx
+++ 07-heroes-app/src/ui/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 export const Navbar = () => {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -44,21 +47,12 @@ export const Navbar = () => {
           >
             Search
           </NavLink>
-
-          {/* <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? 'active' : ''}`
-            }
-            to="/hero"
-          >
-            Hero
-          </NavLink> */}
         </div>
       </div>
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Daniel</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
           <button
             className="nav-item nav-link btn"
             onClick={handleLogout}

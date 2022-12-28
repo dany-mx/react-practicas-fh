c29ef106503f91fd0c5be891706901c5b38324e1
Daniel Gandarillas <gandani.91@gmail.com>
2022-12-28 00:09:59 -0600

    NothingSelectedView en  journalApp

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,12 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views/NothingSelectedView';
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography>
-          Ullamco sunt irure irure nulla. Mollit consectetur velit nisi sint
-          pariatur elit velit sit. Nostrud consectetur qui sunt commodo
-          voluptate.
-        </Typography>
+        <NothingSelectedView />
       </JournalLayout>
     </>
   );

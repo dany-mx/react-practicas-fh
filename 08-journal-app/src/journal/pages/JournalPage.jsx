6e0cdf10fc76723a01c8d4be783e29d4e9d55b9f
Gandarillas <gandani.91@gmail.com>
2022-12-28 17:26:30 -0600

    Note view

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView } from '../views/NoteView';
 import { NothingSelectedView } from '../views/NothingSelectedView';
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <NothingSelectedView />
+        {/* <NothingSelectedView /> */}
+        <NoteView />
       </JournalLayout>
     </>
   );

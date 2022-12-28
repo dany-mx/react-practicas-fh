341d876304a0bfe56d07c0f682613c87c7ebd09a
Gandarillas <gandani.91@gmail.com>
2022-12-28 17:37:13 -0600

    JournalApp - boton flotante

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView } from '../views/NoteView';
 import { NothingSelectedView } from '../views/NothingSelectedView';
@@ -7,8 +8,21 @@ export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        {/* <NothingSelectedView /> */}
-        <NoteView />
+        <NothingSelectedView />
+        {/* <NoteView /> */}
+        <IconButton
+          size="large"
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+        </IconButton>
       </JournalLayout>
     </>
   );

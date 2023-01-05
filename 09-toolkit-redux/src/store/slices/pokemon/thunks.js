23cec51ba3279b3305575288e2c84ec87f835036
Gandarillas <gandani.91@gmail.com>
2023-01-05 10:33:32 -0600

    Redux toolkit - setpokemons fetch

--- 09-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -5,6 +5,12 @@ export const getPokemons = (page = 0) => {
     dispatch(startLoadingPokemons());
     
     //TODO: realizar peticion http
-    // dispatch(setPokemons());
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`);
+    const data = await resp.json();
+    // console.log(data);
+    dispatch(setPokemons({
+      pokemons: data.results,
+      page: page + 1
+    }));
   }
 }

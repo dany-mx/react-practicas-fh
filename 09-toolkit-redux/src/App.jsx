d9eca163c26021a9a33c8258b7994685b2289e89
Gandarillas <gandani.91@gmail.com>
2023-01-04 19:53:18 -0600

    usar valor del store y despachar

--- 09-toolkit-redux/src/App.jsx
+++ 09-toolkit-redux/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { increment } from './store/slices/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <div>
@@ -31,8 +32,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          count is {counter}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR

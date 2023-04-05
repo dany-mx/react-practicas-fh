14af3ea05df1cc8022c82b3d2ac3e9ca8c7f87ef
Gandarillas <gandani.91@gmail.com>
2023-04-05 10:45:30 -0600

    feat: firebase auth google login preparacion

---
+++ 08-journal-app/src/auth/firebase/providers.js
@@ -0,0 +1,27 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try{
+    const result = await signInWithPopup(firebaseAuth, googleProvider);
+    // const credential = GoogleAuthProvider.credentialFromResult(result);
+    const { displayName, email, photoURL, uid } = result.user;
+    console.log(user);
+    return {
+      ok: true,
+      displayName,
+      email,
+      photoURL,
+      uid
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      errorCode: error.code,
+      errorMessage: error.message 
+    }
+  }
+}
